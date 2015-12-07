9a5dab2f65ae383c4396e99237460e1854fddbe0
messina@dmi.unict.it <messina@dmi.unict.it>
2015-12-07 09:45:48 +0100

    modified filenames running scripts

---
+++ run-ciao-W-WI.sh
@@ -0,0 +1,20 @@
+sw=10000
+nt=10
+#dataset=epinions
+dataset=ciao
+
+cd conf/$dataset;
+mkdir -p results/$dataset;
+
+for i in `seq 1 9`; do
+  for j in `seq 1 9`; do
+    f="$dataset-config-WI$j-W$i";
+    fout="$dataset-W0$i-WI0$j-NOoverlap.txt"
+    echo "*** $f *** "
+    rm -f $dataset-config.txt
+    ln -s $f $dataset-config.txt
+    echo "%$f" > $fout
+    java -Xmx6000m -Dsw=$sw -Dnt=$nt  -Ddataset=$dataset -Doverlap=0 Sim 2>>$fout
+    mv $fout results/$dataset/
+  done
+done
