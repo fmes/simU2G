951f0ba0dbb72b25b2388b65b8fd8aa97ebb0c83
messina@dmi.unict.it <messina@dmi.unict.it>
2015-12-09 16:29:08 +0100

    Fixed run scripts

--- run-ciao-W-WI.sh
+++ run-ciao-W-WI.sh
@@ -3,29 +3,34 @@ nt=10
 #dataset=epinions
 dataset=ciao
 
-#compile code
-javac src/Sim.java
+d=$(cd `dirname $0`; pwd)
 
-cd conf/$dataset;
-mkdir -p results/$dataset;
+#compile code
+javac $d/src/Sim.java
+
+#cd conf/$dataset;
+mkdir -p $d/results/$dataset;
+dest=$d/results/$dataset;
+confd=$d/conf/$dataset;
 
 #i is W
 for i in `seq 1 9`; do
   #j is WI
   for j in `seq 1 9`; do
-    f="$dataset-config-WI$j-W$i";
-    fout="$dataset-W0$i-WI0$j-NOoverlap.txt";
-    echo "*** $f *** "
-    rm -f $dataset-config.txt
-    ln -s $f $dataset-config.txt
+    f="$confd/$dataset-config-WI$j-W$i";
+    fout="$confd/$dataset-W0$i-WI0$j-NOoverlap.txt";
+    echo "*** $f ***"
+    rm -f $confd/$dataset-config.txt
+    ln -sf $f $dataset-config.txt
     echo "%$f" > $fout
-    java -cp src -Xmx6000m -Dsw=$sw -Dnt=$nt  -Ddataset=$dataset -Doverlap=0 Sim 2>>$fout
 
-    mkdir -p results/$dataset/W0$i
-    mkdir -p results/$dataset/WI0$j
+    java -cp $d/src -Xmx6000m -Dsw=$sw -Dnt=$nt  -Ddataset=$dataset -Doverlap=0 Sim 2>>$fout
 
-    cp $fout results/$dataset/W0$i
-    cp $fout results/$dataset/WI0$j
+    mkdir -p $dest/W0$i
+    mkdir -p $dest/WI0$j
+
+    cp $fout $dest/W0$i
+    cp $fout $dest/WI0$j
 
     rm $fout
   done
